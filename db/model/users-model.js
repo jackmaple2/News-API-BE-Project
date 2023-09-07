f9ae8269899aabcac522110d4e5a9db614791a43
jackmaple2 <jackmaple3000@gmail.com>
2023-09-07 15:53:48 +0100

    commit fixes user-model error by deletion

--- db/model/users-model.js
+++ db/model/users-model.js
@@ -1,16 +1,16 @@
 const db = require('../connection');
 const { convertTimestampToDate, createRef, formatComments} = require('../seeds/utils');
 
-
 function selectUsers() {
     return db.query(`SELECT * FROM users`)
-    .then(([rows]) => {
-    if (rows.length === 0) {
-        return Promise.reject({status: 404, msg: 'Not found'})
-    }
-        return rows;
+    .then((result) =>  {
+        if (result.rows.length === 0) {
+            return Promise.reject({status: 404, msg: 'Resource not found'})
+        } else {
+            return result.rows;
+        }
     })
-}
+ }
 
 module.exports = {selectUsers}
 
