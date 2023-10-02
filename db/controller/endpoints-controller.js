219bcf2cf332df73c82cab71aaa2116ca6938698
jackmaple2 <jackmaple3000@gmail.com>
2023-10-02 17:47:46 +0100

    updates the endpoints.json file

--- db/controller/endpoints-controller.js
+++ db/controller/endpoints-controller.js
@@ -3,10 +3,8 @@ const db = require('../connection');
 const { convertTimestampToDate, createRef, formatComments} = require('../seeds/utils');
 const jsonEndpoints = require('../../endpoints.json');
 
-
 const getEndpointInformation = (request, response) => {
     response.status(200).send(jsonEndpoints);
-
 }
 
 module.exports = { getEndpointInformation };
